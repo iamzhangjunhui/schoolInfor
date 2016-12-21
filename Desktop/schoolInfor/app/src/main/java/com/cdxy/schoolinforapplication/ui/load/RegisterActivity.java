3898d65116109e21365eaae311d69baa17f34ca6
zhangjh <zhangjh@msltec.cn>
2016-12-21 12:41:25 +0800

    实现了系、班级、民族的选择功能

--- Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/ui/load/RegisterActivity.java
+++ Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/ui/load/RegisterActivity.java
@@ -1,28 +1,121 @@
 package com.cdxy.schoolinforapplication.ui.load;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.cdxy.schoolinforapplication.R;
+import com.cdxy.schoolinforapplication.ui.ChooseInfor.ChooseInforActivity;
 import com.cdxy.schoolinforapplication.ui.base.BaseActivity;
+import com.cdxy.schoolinforapplication.util.Constant;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.img_back)
+    ImageView imgBack;
+    @BindView(R.id.txt_title)
+    TextView txtTitle;
+    @BindView(R.id.btn_right)
+    Button btnRight;
+    @BindView(R.id.edt_realname)
+    EditText edtRealname;
+    @BindView(R.id.txt_department)
+    TextView txtDepartment;
+    @BindView(R.id.txt_class)
+    TextView txtClass;
+    @BindView(R.id.edt_student_id)
+    EditText edtStudentId;
+    @BindView(R.id.girl)
+    RadioButton girl;
+    @BindView(R.id.boy)
+    RadioButton boy;
+    @BindView(R.id.rg_sex)
+    RadioGroup rgSex;
+    @BindView(R.id.txt_birthday)
+    TextView txtBirthday;
+    @BindView(R.id.txt_nation)
+    TextView txtNation;
+    @BindView(R.id.edt_address)
+    EditText edtAddress;
+    @BindView(R.id.edt_hobby)
+    EditText edtHobby;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
         initView();
     }
 
     @Override
     public void initView() {
+    txtTitle.setText("注册");
 
     }
 
     @Override
     public void onClick(View view) {
+     switch (view.getId()){
+         case R.id.img_back:
+             finish();
+             break;
+         case R.id.txt_department:
+             Intent intent=new Intent(RegisterActivity.this, ChooseInforActivity.class);
+             intent.putExtra("flagSelectInforFrom",Constant.FLAG_REQURST_FROM_DEPARTMENT);
+             startActivityForResult(intent, Constant.REQUEST_CODE_CHOOSEDEPARTMENT);
+             break;
+         case R.id.txt_class:
+             intent=new Intent(RegisterActivity.this, ChooseInforActivity.class);
+             intent.putExtra("flagSelectInforFrom",Constant.FLAG_REQURST_FROM_CLASS);
+             String department=txtDepartment.getText().toString();
+             if (TextUtils.isEmpty(department)){
+                 toast("请先选择你所在系");
+                 return;
+             }else {
+                 intent.putExtra("department",department);
+                 startActivityForResult(intent, Constant.REQUEST_CODE_CHOOSECLASS);
+             }
+             break;
+         case R.id.txt_nation:
+             intent=new Intent(RegisterActivity.this, ChooseInforActivity.class);
+             intent.putExtra("flagSelectInforFrom",Constant.FLAG_REQURST_FROM_NATION);
+             startActivityForResult(intent, Constant.REQUEST_CODE_CHOOSENATION);
+             break;
+         default:
+             break;
+     }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==Constant.REQUEST_CODE_CHOOSEDEPARTMENT&&resultCode==Constant.RESULT_CODE_CHOOSEDEPARTMENT) {
+            String department = data.getStringExtra("department");
+            if (!TextUtils.isEmpty(department)) {
+                txtDepartment.setText(department);
+            }
+        }
+        if (requestCode==Constant.REQUEST_CODE_CHOOSECLASS&&resultCode==Constant.RESULT_CODE_CHOOSECLASS){
+            String clazz=data.getStringExtra("clazz");
+            if (!TextUtils.isEmpty(clazz)){
+                    txtClass.setText(clazz);
+            }
+        }
+        if (requestCode==Constant.REQUEST_CODE_CHOOSENATION&&resultCode==Constant.RESULT_CODE_CHOOSENATION);
+             String nation=data.getStringExtra("nation");
+             if (!TextUtils.isEmpty(nation)){
+                     txtNation.setText(nation);
+             }
     }
 }
--- Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/util/Constant.java
+++ Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/util/Constant.java
@@ -5,4 +5,13 @@ package com.cdxy.schoolinforapplication.util;
  */
 
 public class Constant {
+    public static final int REQUEST_CODE_CHOOSEDEPARTMENT=1;
+    public static final int RESULT_CODE_CHOOSEDEPARTMENT=2;
+    public static final int REQUEST_CODE_CHOOSECLASS=3;
+    public static final int RESULT_CODE_CHOOSECLASS=4;
+    public static final int REQUEST_CODE_CHOOSENATION=5;
+    public static final int RESULT_CODE_CHOOSENATION=6;
+    public static final String FLAG_REQURST_FROM_DEPARTMENT="department";
+    public static final String FLAG_REQURST_FROM_CLASS="class";
+    public static final String FLAG_REQURST_FROM_NATION="nation";
 }
