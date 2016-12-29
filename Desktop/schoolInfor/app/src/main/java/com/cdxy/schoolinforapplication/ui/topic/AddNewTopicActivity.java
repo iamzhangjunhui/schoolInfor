7ec754a46b13d3a66b9f94521a4774423899c62f
zhangjh <zhangjh@msltec.cn>
2016-12-29 22:25:06 +0800

    如果定位成功并获得地址描述，添加显示当前地理位置功能，点击显示会直接显示在编辑框中，也可以再次点击取消显示

--- Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/ui/topic/AddNewTopicActivity.java
+++ Desktop/schoolInfor/app/src/main/java/com/cdxy/schoolinforapplication/ui/topic/AddNewTopicActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,13 +21,16 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.Poi;
 import com.cdxy.schoolinforapplication.R;
 import com.cdxy.schoolinforapplication.ScreenManager;
 import com.cdxy.schoolinforapplication.adapter.ShowPhotoAdapter;
 import com.cdxy.schoolinforapplication.ui.base.BaseActivity;
 import com.cdxy.schoolinforapplication.util.Constant;
 import com.cdxy.schoolinforapplication.util.OnRecyclerItemOnLongClickListener;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,8 +52,16 @@ public class AddNewTopicActivity extends BaseActivity implements View.OnClickLis
     LinearLayout activityAddNewTopic;
     @BindView(R.id.edt_new_topic)
     EditText edtNewTopic;
+    @BindView(R.id.txt_remind_show_address)
+    TextView txtRemindShowAddress;
+    @BindView(R.id.txt_now_address)
+    TextView txtNowAddress;
+    @BindView(R.id.layout_show_address)
+    LinearLayout layoutShowAddress;
     private List<Object> list;
     private ShowPhotoAdapter adapter;
+    private String newTopic;
+    private boolean isShowAddress = false;
     public LocationClient locationClient;
     public BDLocationListener myListener = new MyLocationListener();
 
@@ -107,6 +118,7 @@ public class AddNewTopicActivity extends BaseActivity implements View.OnClickLis
             case R.id.img_back:
                 ScreenManager.getScreenManager().popActivty(this);
                 break;
+
         }
     }
 
@@ -157,65 +169,33 @@ public class AddNewTopicActivity extends BaseActivity implements View.OnClickLis
         public void onReceiveLocation(BDLocation location) {
             //Receive Location
             StringBuffer sb = new StringBuffer(256);
-            sb.append("time : ");
-            sb.append(location.getTime());
-            sb.append("\nerror code : ");
-            sb.append(location.getLocType());
-            sb.append("\nlatitude : ");
-            sb.append(location.getLatitude());
-            sb.append("\nlontitude : ");
-            sb.append(location.getLongitude());
-            sb.append("\nradius : ");
-            sb.append(location.getRadius());
-            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-                sb.append("\nspeed : ");
-                sb.append(location.getSpeed());// 单位：公里每小时
-                sb.append("\nsatellite : ");
-                sb.append(location.getSatelliteNumber());
-                sb.append("\nheight : ");
-                sb.append(location.getAltitude());// 单位：米
-                sb.append("\ndirection : ");
-                sb.append(location.getDirection());// 单位度
-                sb.append("\naddr : ");
-                sb.append(location.getAddrStr());
-                sb.append("\ndescribe : ");
-                sb.append("gps定位成功");
-
-            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-                sb.append("\naddr : ");
-                sb.append(location.getAddrStr());
-                //运营商信息
-                sb.append("\noperationers : ");
-                sb.append(location.getOperators());
-                sb.append("\ndescribe : ");
-                sb.append("网络定位成功");
-            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                sb.append("\ndescribe : ");
-                sb.append("离线定位成功，离线定位结果也是有效的");
-            } else if (location.getLocType() == BDLocation.TypeServerError) {
-                sb.append("\ndescribe : ");
-                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                sb.append("\ndescribe : ");
-                sb.append("网络不同导致定位失败，请检查网络是否通畅");
-            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-                sb.append("\ndescribe : ");
-                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-            }
             sb.append("\nlocationdescribe : ");
-            sb.append(location.getLocationDescribe());// 位置语义化信息
-            List<Poi> list = location.getPoiList();// POI数据
-            if (list != null) {
-                sb.append("\npoilist size = : ");
-                sb.append(list.size());
-                for (Poi p : list) {
-                    sb.append("\npoi= : ");
-                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-                }
+            final String address = location.getLocationDescribe();
+            sb.append(address);// 位置语义化信息
+            if (!TextUtils.isEmpty(address)) {
+                layoutShowAddress.setVisibility(View.VISIBLE);
+                txtNowAddress.setText(address);
+                txtRemindShowAddress.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (isShowAddress == false) {
+                            isShowAddress = true;
+                            newTopic = edtNewTopic.getText().toString();//创建话题编辑框的内容
+                            txtRemindShowAddress.setText("取消显示当前地址");
+                            edtNewTopic.setText(newTopic + "   --" + address);
+                        } else {
+                            isShowAddress = false;
+                            newTopic = edtNewTopic.getText().toString();
+                            txtRemindShowAddress.setText("显示当前地址");
+                            String newTopicContent = newTopic.substring(0, newTopic.lastIndexOf("--") - 1);
+                            edtNewTopic.setText(newTopicContent + "");
+                        }
+                    }
+                });
             }
-            Log.i("BaiduLocationApiDem", sb.toString());
-            edtNewTopic.setText(sb.toString());
+
 
         }
     }
+
 }
